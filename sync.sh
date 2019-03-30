f126515f6d0139cdc123827bbec39d3e244dbd83
Khue Doan <khuedoan98@gmail.com>
2019-03-30 20:06:21 +0700

    Clean up

--- sync.sh
+++ sync.sh
@@ -1,16 +1,18 @@
-BACKUPDIR=$HOME/.dotfiles/backup
+REPO="https://github.com/khuedoan98/dotfiles.git"
+GITDIR=$HOME/.dotfiles/
+BACKUPDIR=$HOME/.dotfiles_backup/
 
-git clone --bare https://github.com/khuedoan98/dotfiles.git $HOME/.dotfiles
+git clone --bare $REPO $GITDIR
 
 function dotfiles {
-   /usr/bin/git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME $@
+   /usr/bin/git --git-dir=$GITDIR --work-tree=$HOME $@
 }
 
 echo "Checking out dotfiles..."
 if ! dotfiles checkout; then
     echo "Found existing dotfiles, backing up...";
     mkdir -p $BACKUPDIR
-    dotfiles checkout 2>&1 | egrep "\s+\." | awk {'print $1'} | xargs -I{} mv {} .dotfiles-backup/{}
+    dotfiles checkout 2>&1 | egrep "\s+\." | awk {'print $1'} | xargs -I{} mv {} $BACKUPDIR{}
     dotfiles checkout
 fi;
 echo "Checked out dotfiles.";

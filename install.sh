3e5abd933d16b7b6064810319fa36d0f049f1f27
Khue Doan <khuedoan98@gmail.com>
2019-04-01 09:25:34 +0700

    Auto create parent directory when backup

--- install.sh
+++ install.sh
@@ -8,13 +8,24 @@ function dotfiles {
    /usr/bin/git --git-dir=$GITDIR --work-tree=$HOME $@
 }
 
+function mv () {
+    dir="$2"
+    tmp="$2"; tmp="${tmp: -1}"
+    [ "$tmp" != "/" ] && dir="$(dirname "$2")"
+    [ -a "$dir" ] ||
+    mkdir -p "$dir" &&
+    mv "$@"
+}
+
 echo "Checking out dotfiles..."
 if ! dotfiles checkout; then
     echo "Found existing dotfiles, backing up...";
     mkdir -p $BACKUPDIR
-    dotfiles checkout 2>&1 | egrep "\s+\." | awk {'print $1'} | xargs -I{} mv {} $BACKUPDIR{}
+    dotfiles checkout 2>&1 | egrep "\s+\." | awk {'print $1'} | xargs -I{} mv -p {} $BACKUPDIR{}
     dotfiles checkout
+    echo "Checked out dotfiles.";
+else
+    echo "Checked out dotfiles.";
 fi;
-echo "Checked out dotfiles.";
 
 dotfiles config status.showUntrackedFiles no

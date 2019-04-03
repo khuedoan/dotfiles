99bc66ca3d2a055acfc56518911455c998104c68
Khue Doan <khuedoan98@gmail.com>
2019-04-04 00:46:32 +0700

    Remove auto backup

--- install.sh
+++ install.sh
@@ -8,16 +8,9 @@ function dotfiles {
    /usr/bin/git --git-dir=$GITDIR --work-tree=$HOME $@
 }
 
-echo "Checking out dotfiles..."
-if ! dotfiles checkout; then
-    echo "Found existing dotfiles, backing up...";
-    mkdir -p $BACKUPDIR
-    dotfiles checkout 2>&1 | egrep "\s+\." | awk {'print $1'} | xargs dirname | xargs -I{} mkdir -p {}
-    dotfiles checkout 2>&1 | egrep "\s+\." | awk {'print $1'} | xargs -I{} mv {} $BACKUPDIR{}
-    dotfiles checkout
-    echo "Checked out dotfiles.";
-else
-    echo "Checked out dotfiles.";
-fi;
-
 dotfiles config status.showUntrackedFiles no
+
+if ! dotfiles checkout; then
+    rm -rf $GITDIR
+    echo "Please backup or remove the above files and run the script again"
+fi

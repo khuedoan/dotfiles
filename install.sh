855b3bae3c1cba7e9f96dd0e6778325637e32514
Khue Doan <khuedoan98@gmail.com>
2019-04-19 01:13:17 +0700

    Remove backup feature

--- install.sh
+++ install.sh
@@ -1,6 +1,5 @@
 REPO="https://github.com/khuedoan98/dotfiles.git"
 GITDIR=$HOME/.dotfiles/
-BACKUPDIR=$HOME/.dotfilesbackup/
 
 git clone --bare $REPO $GITDIR
 
@@ -8,18 +7,8 @@ function dotfiles {
     /usr/bin/git --git-dir=$GITDIR --work-tree=$HOME $@
 }
 
-function mvp ()
-{
-    dir="$2"
-    tmp="$2"; tmp="${tmp: -1}"
-    [ "$tmp" != "/" ] && dir="$(dirname "$2")"
-    [ -a "$dir" ] ||
-    mkdir -p "$dir" &&
-    mv "$@"
-}
-
 if ! dotfiles checkout; then
-    dotfiles checkout 2>&1 | egrep "\s+\." | awk {'print $1'} | xargs -I{} mvp {} $BACKUPDIR{}
+    dotfiles checkout 2>&1 | egrep "^\s+" | awk {'print $1'} | xargs -I {} rm {}
     dotfiles checkout
 fi
 

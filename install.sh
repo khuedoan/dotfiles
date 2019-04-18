55700e6d404e5a3a6ae5b48869a7e5314129282f
Khuê Đoàn <khuedoan98@gmail.com>
2019-04-18 17:57:55 +0000

    Auto create parent directory when mv

--- install.sh
+++ install.sh
@@ -8,8 +8,18 @@ function dotfiles {
     /usr/bin/git --git-dir=$GITDIR --work-tree=$HOME $@
 }
 
+function mvp ()
+{
+    dir="$2"
+    tmp="$2"; tmp="${tmp: -1}"
+    [ "$tmp" != "/" ] && dir="$(dirname "$2")"
+    [ -a "$dir" ] ||
+    mkdir -p "$dir" &&
+    mv "$@"
+}
+
 if ! dotfiles checkout; then
-    dotfiles checkout 2>&1 | egrep "\s+" | awk {'print $1'} | xargs -I{} mv {} $BACKUPDIR{}
+    dotfiles checkout 2>&1 | egrep "\s+" | awk {'print $1'} | xargs -I{} mvp {} $BACKUPDIR{}
     dotfiles checkout
 fi
 

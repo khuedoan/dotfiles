e61fde3ce6e56bdb5a293583109d649497b56665
Khue Doan <khuedoan98@gmail.com>
2019-04-23 15:51:58 +0700

    Change installation install order

--- install.sh
+++ install.sh
@@ -1,27 +1,10 @@
 #!/bin/dash
 
-# Change default apps here
+# Default apps
 ##########################
 terminal="rxvt-unicode"
 browser="firefox"
 ##########################
-
-echo -n "Install recommended packages? (y/N) "
-read response
-if [ $response = "y" ]; then
-    echo "Installing basic packages"
-    sudo pacman -S xorg-server xorg-xinit bspwm sxhkd $terminal
-    trizen -S polybar dmenu2 i3lock-next-git compton-tryone-git
-    echo "Installing fonts"
-    sudo pacman -S ttf-dejavu
-    trizen -S nerd-fonts-source-code-pro ttf-mac-fonts ttf-ms-fonts
-    echo "Installing extra packages"
-    sudo pacman -S alsa-utils xorg-xbacklight maim xclip dunst feh bc translation-shell playerctl \
-                   htop glances
-    sudo pacman -S $browser pcmanfm zathura zathura-pdf-mupdf mpv youtube-dl
-    sudo pacman -S fzf zsh-auto-suggestions zsh-syntax-highlighting
-    trizen -S zsh-theme-powerlevel10k-git
-fi
 
 REPO="https://github.com/khuedoan98/dotfiles.git"
 GITDIR=$HOME/.dotfiles/
@@ -44,5 +27,26 @@ if ! dotfiles checkout; then
     else
             rm -rf $GITDIR
             echo "Installation cancelled"
+            exit 1
     fi
 fi
+
+echo -n "Install recommended packages? (y/N) "
+read response
+if [ $response = "y" ]; then
+    echo "Installing basic packages"
+    sudo pacman -S xorg-server xorg-xinit bspwm sxhkd
+    trizen -S polybar dmenu2 i3lock-next-git compton-tryone-git
+    echo "Installing fonts"
+    sudo pacman -S ttf-dejavu
+    trizen -S nerd-fonts-source-code-pro ttf-mac-fonts ttf-ms-fonts
+    echo "Installing extra packages"
+    sudo pacman -S alsa-utils xorg-xbacklight maim xclip dunst feh bc translation-shell playerctl \
+                   htop glances
+    sudo pacman -S $terminal $browser
+    sudo pacman -S pcmanfm zathura zathura-pdf-mupdf mpv youtube-dl
+    sudo pacman -S fzf zsh-auto-suggestions zsh-syntax-highlighting
+    trizen -S zsh-theme-powerlevel10k-git
+fi
+
+exit 0

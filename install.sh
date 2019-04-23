cb8697fada1af60b163b6cebb1a3586e6e89f6f2
Khue Doan <khuedoan98@gmail.com>
2019-04-23 14:14:58 +0700

    Interactive and verbose when remove old files

--- install.sh
+++ install.sh
@@ -13,7 +13,7 @@ if ! dotfiles checkout; then
     echo -n "All of the above files will be deleted, are you sure? (y/N) "
     read response
     if [ $response = "y" ]; then
-            dotfiles checkout 2>&1 | egrep "^\s+" | awk {'print $1'} | xargs -I {} rm {}
+            dotfiles checkout 2>&1 | egrep "^\s+" | awk {'print $1'} | xargs -I {} rm -iv {}
             dotfiles checkout &&
             dotfiles config status.showUntrackedFiles no &&
             dotfiles push --set-upstream origin master &&

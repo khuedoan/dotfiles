23b41115d3d237ae9ee8aca9ccc0ae08316142d6
Khue Doan <khuedoan98@gmail.com>
2019-05-03 16:59:21 +0700

    Fix answer "n" not showing intel prompt after bumblebee

--- install.sh
+++ install.sh
@@ -2,7 +2,7 @@
 
 echo -n "Install trizen (AUR helper)? (y/N) " && read aur
 echo -n "Install bumblebee (for optimus NVIDIA card)? (y/N) " && read bb
-[ "$bb" ] || (echo -n "Install Intel graphics driver? (y/N) " && read intel)
+[ "$bb" = "y" ] || (echo -n "Install Intel graphics driver? (y/N) " && read intel)
 echo -n "Install recommended packages? (y/N) " && read pkg
 echo -n "Install Vietnamese input method (fcitx-unikey)? (y/N) " && read vnim
 echo -n "Install zsh plugins? (y/N) " && read zplug

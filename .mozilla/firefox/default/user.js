f763f1dd3d97b5b2b49a6aa8a601daa38900615d
Khue Doan <khuedoan98@gmail.com>
2019-05-19 00:37:54 +0700

    Remove custom search region

--- .mozilla/firefox/default/user.js
+++ .mozilla/firefox/default/user.js
@@ -1,21 +1,20 @@
-user_pref("extensions.pocket.enabled",            false);
-user_pref("full-screen-api.warning.delay",        0);
-user_pref("full-screen-api.warning.timeout",      0);
-user_pref("media.webm.enabled",                   false);
-user_pref("browser.urlbar.oneOffSearches",        false);
-user_pref("browser.urlbar.clickSelectsAll",       true);
-user_pref("browser.urlbar.doubleClickSelectsAll", false);
-user_pref("toolkit.cosmeticAnimations.enabled",   false);
-user_pref("privacy.resistFingerprinting",         true);
+user_pref("extensions.pocket.enabled",                                   false);
+user_pref("full-screen-api.warning.delay",                               0);
+user_pref("full-screen-api.warning.timeout",                             0);
+user_pref("media.webm.enabled",                                          false);
+user_pref("browser.urlbar.oneOffSearches",                               false);
+user_pref("browser.urlbar.clickSelectsAll",                              true);
+user_pref("browser.urlbar.doubleClickSelectsAll",                        false);
+user_pref("toolkit.cosmeticAnimations.enabled",                          false);
+user_pref("privacy.resistFingerprinting",                                true);
 
-user_pref("general.smoothScroll",                 false);
-user_pref("browser.ctrlTab.recentlyUsedOrder",    false);
-user_pref("browser.download.useDownloadDir", false);
+user_pref("general.smoothScroll",                                        false);
+user_pref("browser.ctrlTab.recentlyUsedOrder",                           false);
+user_pref("browser.download.useDownloadDir",                             false);
 user_pref("browser.newtabpage.activity-stream.feeds.section.highlights", false);
-user_pref("browser.newtabpage.activity-stream.feeds.snippets", false);
-user_pref("browser.newtabpage.activity-stream.showSearch", false);
-user_pref("browser.newtabpage.activity-stream.topSitesRows", 4);
-user_pref("browser.search.region", "VN");
+user_pref("browser.newtabpage.activity-stream.feeds.snippets",           false);
+user_pref("browser.newtabpage.activity-stream.showSearch",               false);
+user_pref("browser.newtabpage.activity-stream.topSitesRows",             4);
 
 user_pref("browser.uiCustomization.state", "{\"placements\":{\"widget-overflow-fixed-list\":[\"https-everywhere_eff_org-browser-action\",\"_29607f9b-5198-4832-9ea9-16085d102734_-browser-action\",\"ublock0_raymondhill_net-browser-action\",\"_d7742d87-e61d-4b78-b8a1-b469842139fa_-browser-action\",\"_6acd0f4d-ab79-4b79-9b28-8bde65ae355c_-browser-action\"],\"nav-bar\":[\"back-button\",\"forward-button\",\"stop-reload-button\",\"urlbar-container\",\"downloads-button\"],\"toolbar-menubar\":[\"menubar-items\"],\"TabsToolbar\":[\"tabbrowser-tabs\",\"new-tab-button\",\"alltabs-button\"],\"PersonalToolbar\":[\"personal-bookmarks\"]},\"seen\":[\"developer-button\",\"ublock0_raymondhill_net-browser-action\",\"_d7742d87-e61d-4b78-b8a1-b469842139fa_-browser-action\",\"_29607f9b-5198-4832-9ea9-16085d102734_-browser-action\",\"https-everywhere_eff_org-browser-action\",\"webide-button\",\"_6acd0f4d-ab79-4b79-9b28-8bde65ae355c_-browser-action\"],\"dirtyAreaCache\":[\"nav-bar\",\"toolbar-menubar\",\"TabsToolbar\",\"PersonalToolbar\",\"widget-overflow-fixed-list\"],\"currentVersion\":15,\"newElementCount\":4}");
 user_pref("browser.uidensity", 1);

6f9db6edcbe2a984aac200f33ceb87f4ea296715
Khue Doan <khuedoan98@gmail.com>
2020-01-30 14:04:04 +0700

    Change browser to qutebrowser

--- install.sh
+++ install.sh
@@ -36,7 +36,7 @@ install_aur_helper() {
 }
 
 install_core_packages() {
-    sudo pacman --noconfirm --needed -S alsa-utils bc bspwm dunst feh fzf git libnotify maim npm playerctl sxhkd translate-shell ttf-dejavu wmctrl xautolock xcape xclip xdotool xorg-server xorg-setxkbmap xorg-xbacklight xorg-xinit xorg-xrandr xorg-xsetroot xss-lock
+    sudo pacman --noconfirm --needed -S alsa-utils bc bspwm dunst feh fzf git libnotify maim npm playerctl qutebrowser sxhkd translate-shell ttf-dejavu wmctrl xautolock xcape xclip xdotool xorg-server xorg-setxkbmap xorg-xbacklight xorg-xinit xorg-xrandr xorg-xsetroot xss-lock
     trizen --noconfirm --needed -S nerd-fonts-source-code-pro polybar ttf-mac-fonts
 
     # zsh plugins
@@ -77,8 +77,7 @@ install_core_packages() {
 
 install_extra_packages() {
     sudo pacman --noconfirm --needed -S arc-gtk-theme aria2 glances gvfs htop mpv noto-fonts noto-fonts-cjk noto-fonts-emoji noto-fonts-extra ntfs-3g papirus-icon-theme pcmanfm-gtk3 ranger tmux tree unrar unzip w3m xarchiver xdg-user-dirs-gtk youtube-dl zathura zathura-pdf-mupdf zip
-    gpg --recv-keys EB4F9E5A60D32232BB52150C12C87A28FEAC6B20
-    trizen --noconfirm --needed -S chromium-vaapi-bin ttf-ms-fonts
+    trizen --noconfirm --needed -S ttf-ms-fonts
     xdg-user-dirs-update
 }
 

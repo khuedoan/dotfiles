85ecca1772871c7c44df7e4ac02ff0dbe00e1f59
Khue Doan <khuedoan98@gmail.com>
2020-01-30 14:04:04 +0700

    Change browser to qutebrowser

---
+++ .config/qutebrowser/config.py
@@ -0,0 +1,11 @@
+import dracula
+
+# Load existing settings made via :set
+config.load_autoconfig()
+
+dracula.blood(c, {
+    'spacing': {
+        'vertical': 6,
+        'horizontal': 8
+    }
+})

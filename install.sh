1e17bff6d6a842a789f55f401402b9f111677fec
Khue Doan <khuedoan98@gmail.com>
2020-07-02 17:03:48 +0700

    Remove chromium-vaapi key

--- install.sh
+++ install.sh
@@ -84,7 +84,6 @@ install_core_packages() {
 
 install_extra_packages() {
     sudo pacman --noconfirm --needed -S arc-gtk-theme aria2 glances gvfs htop man mpv noto-fonts noto-fonts-cjk noto-fonts-emoji noto-fonts-extra ntfs-3g papirus-icon-theme pcmanfm-gtk3 ranger tmux tree unrar unzip w3m xarchiver youtube-dl zathura zathura-pdf-mupdf zip
-    gpg --recv-keys EB4F9E5A60D32232BB52150C12C87A28FEAC6B20
     trizen --noconfirm --needed -S ttf-ms-fonts ibus-bamboo
     dconf load /desktop/ibus/ < $HOME/.config/ibus/ibus.dconf
 }

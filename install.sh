eae61c63743b8aaa2957ebb28a4a9563db801b2c
Khue Doan <khuedoan98@gmail.com>
2020-07-18 12:25:49 +0700

    Add thefuck package

--- install.sh
+++ install.sh
@@ -83,7 +83,7 @@ install_core_packages() {
 }
 
 install_extra_packages() {
-    sudo pacman --noconfirm --needed -S arc-gtk-theme aria2 glances gvfs htop man mpv noto-fonts noto-fonts-cjk noto-fonts-emoji noto-fonts-extra ntfs-3g papirus-icon-theme pcmanfm-gtk3 ranger tmux tree unrar unzip w3m xarchiver youtube-dl zathura zathura-pdf-mupdf zip
+    sudo pacman --noconfirm --needed -S arc-gtk-theme aria2 glances gvfs htop man mpv noto-fonts noto-fonts-cjk noto-fonts-emoji noto-fonts-extra ntfs-3g papirus-icon-theme pcmanfm-gtk3 ranger thefuck tmux tree unrar unzip w3m xarchiver youtube-dl zathura zathura-pdf-mupdf zip
     trizen --noconfirm --needed -S ttf-ms-fonts ibus-bamboo
     dconf load /desktop/ibus/ < $HOME/.config/ibus/ibus.dconf
 }

8ad9c5894e6b08500fc21409ab423ef04c656f62
Khue Doan <khuedoan98@gmail.com>
2020-07-27 23:48:23 +0700

    Cyan Arch logo

--- .p10k.zsh
+++ .p10k.zsh
@@ -173,7 +173,7 @@
   #################################[ os_icon: os identifier ]##################################
   # OS identifier color.
   typeset -g POWERLEVEL9K_OS_ICON_FOREGROUND=232
-  typeset -g POWERLEVEL9K_OS_ICON_BACKGROUND=7
+  typeset -g POWERLEVEL9K_OS_ICON_BACKGROUND=6
   # Custom icon.
   # typeset -g POWERLEVEL9K_OS_ICON_CONTENT_EXPANSION='⭐'
 

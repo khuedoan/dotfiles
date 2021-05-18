8f4596dc4dc88ef8f0cff5914ed49231fe95e598
Khue Doan <khuedoan98@gmail.com>
2021-05-18 21:10:06 +0700

    Add LSP auto install and completion

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -89,12 +89,14 @@ plug {'bronson/vim-visual-star-search'}
 plug {'christoomey/vim-tmux-navigator'}
 plug {'farmergreg/vim-lastplace'}
 plug {'hoob3rt/lualine.nvim'}
+plug {'hrsh7th/nvim-compe'}
 plug {'iamcco/markdown-preview.nvim', run=vim.fn['mkdp#util#install']}
 plug {'jiangmiao/auto-pairs'}
 plug {'joshdick/onedark.vim'}
 plug {'junegunn/fzf'}
 plug {'junegunn/fzf.vim'}
 plug {'justinmk/vim-sneak'}
+plug {'kabouzeid/nvim-lspinstall'}
 plug {'kyazdani42/nvim-web-devicons'}
 plug {'lukas-reineke/indent-blankline.nvim', branch='lua'} -- TODO use master branch when 0.5 is out
 plug {'mcchrish/nnn.vim'}
@@ -173,13 +175,22 @@ map('n', '<LEADER>gt', ':GFiles!<CR>',      {noremap = true})
 
 -- Language servers
 
--- TODO auto install all language servers
+-- TODO optimize LSP
 
 local servers = {
   "pyright",
   "yamlls"
 }
 
+local function setup_servers()
+  require'lspinstall'.setup()
+  local servers = require'lspinstall'.installed_servers()
+  for _, server in pairs(servers) do
+    require'lspconfig'[server].setup{}
+  end
+end
+
+setup_servers()
 for _, lsp in ipairs(servers) do
   require('lspconfig')[lsp].setup{
     on_attach = require('completion').on_attach
@@ -188,6 +199,64 @@ end
 
 map('i', '<TAB>',   '<Plug>(completion_smart_tab)',   {})
 map('i', '<S-TAB>', '<Plug>(completion_smart_s_tab)', {})
+
+require'compe'.setup {
+  enabled = true;
+  autocomplete = true;
+  debug = false;
+  min_length = 1;
+  preselect = 'enable';
+  throttle_time = 80;
+  source_timeout = 200;
+  incomplete_delay = 400;
+  max_abbr_width = 100;
+  max_kind_width = 100;
+  max_menu_width = 100;
+  documentation = true;
+
+  source = {
+    path = true;
+    nvim_lsp = true;
+  };
+}
+
+local t = function(str)
+  return vim.api.nvim_replace_termcodes(str, true, true, true)
+end
+
+local check_back_space = function()
+    local col = vim.fn.col('.') - 1
+    if col == 0 or vim.fn.getline('.'):sub(col, col):match('%s') then
+        return true
+    else
+        return false
+    end
+end
+
+-- Use (s-)tab to:
+--- move to prev/next item in completion menuone
+--- jump to prev/next snippet's placeholder
+_G.tab_complete = function()
+  if vim.fn.pumvisible() == 1 then
+    return t "<C-n>"
+  elseif check_back_space() then
+    return t "<Tab>"
+  else
+    return vim.fn['compe#complete']()
+  end
+end
+_G.s_tab_complete = function()
+  if vim.fn.pumvisible() == 1 then
+    return t "<C-p>"
+  else
+    return t "<S-Tab>"
+  end
+end
+
+vim.api.nvim_set_keymap("i", "<Tab>", "v:lua.tab_complete()", {expr = true})
+vim.api.nvim_set_keymap("s", "<Tab>", "v:lua.tab_complete()", {expr = true})
+vim.api.nvim_set_keymap("i", "<S-Tab>", "v:lua.s_tab_complete()", {expr = true})
+vim.api.nvim_set_keymap("s", "<S-Tab>", "v:lua.s_tab_complete()", {expr = true})
 
 -- Sneak
 

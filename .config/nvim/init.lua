6c5016782608a51a446fab457244a5aa38a1e4c0
Khue Doan <khuedoan98@gmail.com>
2021-05-19 11:19:13 +0700

    Simplify tab complete key map

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -191,14 +191,12 @@ local function setup_servers()
 end
 
 setup_servers()
+
 for _, lsp in ipairs(servers) do
   require('lspconfig')[lsp].setup{
     on_attach = require('completion').on_attach
   }
 end
-
-map('i', '<TAB>',   '<Plug>(completion_smart_tab)',   {})
-map('i', '<S-TAB>', '<Plug>(completion_smart_s_tab)', {})
 
 require'compe'.setup {
   enabled = true;
@@ -221,43 +219,8 @@ require'compe'.setup {
   };
 }
 
-local t = function(str)
-  return vim.api.nvim_replace_termcodes(str, true, true, true)
-end
-
-local check_back_space = function()
-    local col = vim.fn.col('.') - 1
-    if col == 0 or vim.fn.getline('.'):sub(col, col):match('%s') then
-        return true
-    else
-        return false
-    end
-end
-
--- Use (s-)tab to:
---- move to prev/next item in completion menuone
---- jump to prev/next snippet's placeholder
-_G.tab_complete = function()
-  if vim.fn.pumvisible() == 1 then
-    return t "<C-n>"
-  elseif check_back_space() then
-    return t "<Tab>"
-  else
-    return vim.fn['compe#complete']()
-  end
-end
-_G.s_tab_complete = function()
-  if vim.fn.pumvisible() == 1 then
-    return t "<C-p>"
-  else
-    return t "<S-Tab>"
-  end
-end
-
-vim.api.nvim_set_keymap("i", "<Tab>", "v:lua.tab_complete()", {expr = true})
-vim.api.nvim_set_keymap("s", "<Tab>", "v:lua.tab_complete()", {expr = true})
-vim.api.nvim_set_keymap("i", "<S-Tab>", "v:lua.s_tab_complete()", {expr = true})
-vim.api.nvim_set_keymap("s", "<S-Tab>", "v:lua.s_tab_complete()", {expr = true})
+map('i', '<TAB>',   '<Plug>(completion_smart_tab)',   {})
+map('i', '<S-TAB>', '<Plug>(completion_smart_s_tab)', {})
 
 -- Sneak
 

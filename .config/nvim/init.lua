d7dd46f58543a48a407e45a991c1a443c1c2ccae
Khue Doan <khuedoan98@gmail.com>
2021-05-20 15:37:08 +0700

    Add tab complete back

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -202,7 +202,47 @@ require('compe').setup({
   };
 })
 
-map('i', '<CR>', 'compe#confirm("<CR>")', {expr = true, noremap = true, silent = true})
+-- TODO clean up tab complete
+
+local t = function(str)
+    return vim.api.nvim_replace_termcodes(str, true, true, true)
+end
+
+local check_back_space = function()
+    local col = vim.fn.col(".") - 1
+    if col == 0 or vim.fn.getline("."):sub(col, col):match("%s") then
+        return true
+    else
+        return false
+    end
+end
+
+_G.tab_complete = function()
+    if vim.fn.call("vsnip#jumpable", {1}) == 1 then
+        return t "<Plug>(vsnip-jump-next)"
+    elseif vim.fn.pumvisible() == 1 then
+        return t "<C-n>"
+    elseif check_back_space() then
+        return t "<Tab>"
+    else
+        return vim.fn["compe#complete"]()
+    end
+end
+_G.s_tab_complete = function()
+		if vim.fn.call("vsnip#jumpable", {-1}) == 1 then
+				return t "<Plug>(vsnip-jump-prev)"
+    elseif vim.fn.pumvisible() == 1 then
+        return t "<C-p>"
+    else
+        return t "<S-Tab>"
+    end
+end
+
+map("i", "<CR>", "compe#confirm('<CR>')",     {expr = true})
+map("i", "<Tab>", "v:lua.tab_complete()",     {expr = true})
+map("s", "<Tab>", "v:lua.tab_complete()",     {expr = true})
+map("i", "<S-Tab>", "v:lua.s_tab_complete()", {expr = true})
+map("s", "<S-Tab>", "v:lua.s_tab_complete()", {expr = true})
 
 -- Sneak
 

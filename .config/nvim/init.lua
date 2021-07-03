1ed2efec31279286dc838e43f4dec0148d5bbe8f
Khue Doan <khuedoan98@gmail.com>
2021-07-03 18:51:08 +0700

    Switch plugin manager to packer
    Got some bugs in paq-nvim and packer has declarative plugin specification

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,35 +1,25 @@
 -------------
--- Aliases --
--------------
-
-local cmd = vim.cmd
-local fn = vim.fn
-local g = vim.g
-local map = vim.api.nvim_set_keymap
-local o = vim.opt
-
--------------
 -- Options --
 -------------
 
-o.completeopt = 'menuone,noinsert,noselect'
-o.cursorline = true
-o.expandtab = true
-o.inccommand = 'nosplit'
-o.mouse = 'a'
-o.number = true
-o.relativenumber = true
-o.scrolloff = 1
-o.shiftwidth = 4
-o.shortmess = o.shortmess + 'c'
-o.showmode = false
-o.signcolumn = 'yes'
-o.splitbelow = true
-o.splitright = true
-o.tabstop = 4
-o.termguicolors = true
-o.undofile = true
-o.updatetime = 100
+vim.opt.completeopt = 'menuone,noinsert,noselect'
+vim.opt.cursorline = true
+vim.opt.expandtab = true
+vim.opt.inccommand = 'nosplit'
+vim.opt.mouse = 'a'
+vim.opt.number = true
+vim.opt.relativenumber = true
+vim.opt.scrolloff = 1
+vim.opt.shiftwidth = 4
+vim.opt.shortmess = vim.opt.shortmess + 'c'
+vim.opt.showmode = false
+vim.opt.signcolumn = 'yes'
+vim.opt.splitbelow = true
+vim.opt.splitright = true
+vim.opt.tabstop = 4
+vim.opt.termguicolors = true
+vim.opt.undofile = true
+vim.opt.updatetime = 100
 
 --------------
 -- Mappings --
@@ -37,16 +27,16 @@ o.updatetime = 100
 
 -- Leader
 
-g.mapleader = ' '
+vim.g.mapleader = ' '
 
 -- Copy and paste with system clipboard
 
-map('v', '<C-c>', '"+y',    {noremap = true})
-map('i', '<C-v>', '<C-r>+', {noremap = true})
+vim.api.nvim_set_keymap('v', '<C-c>', '"+y',    {noremap = true})
+vim.api.nvim_set_keymap('i', '<C-v>', '<C-r>+', {noremap = true})
 
 -- Replace
 
-map('n', '<LEADER>r', ':%s///g<LEFT><LEFT>',  {noremap = true})
+vim.api.nvim_set_keymap('n', '<LEADER>r', ':%s///g<LEFT><LEFT>',  {noremap = true})
 
 -------------
 -- Plugins --
@@ -54,210 +44,203 @@ map('n', '<LEADER>r', ':%s///g<LEFT><LEFT>',  {noremap = true})
 
 -- Auto install plugin manager
 
-local install_path = fn.stdpath('data')..'/site/pack/paqs/opt/paq-nvim'
+local install_path = vim.fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
 
-if fn.empty(fn.glob(install_path)) > 0 then
-    cmd('!git clone --depth 1 https://github.com/savq/paq-nvim.git '..install_path)
+if vim.fn.empty(vim.fn.glob(install_path)) > 0 then
+  vim.fn.system({'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path})
 end
 
-cmd 'packadd paq-nvim'
-local plug = require('paq-nvim').paq
+-- Plugins configurations
 
--- Let the plugin manager manage itself
+return require('packer').startup({function()
+  -- Let packer manage itself
+  use {
+    'wbthomason/packer.nvim',
+    config = function()
+      vim.cmd "autocmd BufWritePost init.lua PackerCompile"
+    end
+  }
 
-plug {'savq/paq-nvim', opt=true}
+  -- Status line
+  use {
+    'hoob3rt/lualine.nvim',
+    config = function()
+      require('lualine').setup({
+        options = {
+          theme = 'onedark'
+        }
+      })
+    end
+  }
 
--- Plugin list
+  -- Git
+  use {
+    'lewis6991/gitsigns.nvim',
+    requires = {
+      'nvim-lua/plenary.nvim'
+    },
+    config = function()
+      require('gitsigns').setup({
+      })
+    end
+  }
 
-plug {'APZelos/blamer.nvim'}
-plug {'bronson/vim-visual-star-search'}
-plug {'christoomey/vim-tmux-navigator'}
-plug {'farmergreg/vim-lastplace'}
-plug {'hoob3rt/lualine.nvim'}
-plug {'hrsh7th/nvim-compe'}
-plug {'hrsh7th/vim-vsnip'}
-plug {'hrsh7th/vim-vsnip-integ'}
-plug {'iamcco/markdown-preview.nvim', run=vim.fn['mkdp#util#install']}
-plug {'jiangmiao/auto-pairs'}
-plug {'joshdick/onedark.vim'}
-plug {'junegunn/fzf'}
-plug {'junegunn/fzf.vim'}
-plug {'kabouzeid/nvim-lspinstall'}
-plug {'kyazdani42/nvim-web-devicons'}
-plug {'lewis6991/gitsigns.nvim'}
-plug {'lukas-reineke/indent-blankline.nvim', branch='lua'} -- TODO use master branch when 0.5 is out
-plug {'mcchrish/nnn.vim'}
-plug {'neovim/nvim-lspconfig'}
-plug {'norcalli/nvim-colorizer.lua'}
-plug {'nvim-lua/plenary.nvim'}
--- plug {'nvim-treesitter/nvim-treesitter'}
-plug {'phaazon/hop.nvim'}
-plug {'rafamadriz/friendly-snippets'}
-plug {'romainl/vim-cool'}
-plug {'romgrk/barbar.nvim'}
-plug {'sheerun/vim-polyglot'}
-plug {'tpope/vim-commentary'}
-plug {'tpope/vim-fugitive'}
-plug {'tpope/vim-surround'}
+  use {
+    'APZelos/blamer.nvim',
+    config = function()
+      vim.g.blamer_enabled = 1
+      vim.g.blamer_show_in_visual_modes = 0
+      vim.g.blamer_relative_time = 1
+    end
+  }
 
--- Auto install and clean plugins
+  use {
+    'tpope/vim-fugitive',
+    config = function()
+      vim.api.nvim_set_keymap('n', '<LEADER>gg', ':Git<SPACE>',      {noremap = true})
+      vim.api.nvim_set_keymap('n', '<LEADER>gs', ':Git<CR>',         {noremap = true})
+      vim.api.nvim_set_keymap('n', '<LEADER>gb', ':Git blame<CR>',   {noremap = true})
+      vim.api.nvim_set_keymap('n', '<LEADER>gc', ':Git commit<CR>',  {noremap = true})
+      vim.api.nvim_set_keymap('n', '<LEADER>gd', ':Gvdiffsplit<CR>', {noremap = true})
+      vim.api.nvim_set_keymap('n', '<LEADER>gl', ':Gclog<CR>',       {noremap = true})
+      vim.api.nvim_set_keymap('n', '<LEADER>gp', ':Git push<CR>',    {noremap = true})
+    end
+  }
 
-require('paq-nvim').install()
-require('paq-nvim').clean()
+  -- Hop
+  use {
+    'phaazon/hop.nvim',
+    config = function()
+      require('hop').setup({})
+      vim.api.nvim_set_keymap('n', 's', ":lua require'hop'.hint_char2()<CR>", {})
+      vim.api.nvim_set_keymap('v', 's', ":lua require'hop'.hint_char2()<CR>", {})
+    end
+  }
 
--------------------
--- Plugin config --
--------------------
+  -- Theme
+  use {
+    'joshdick/onedark.vim',
+    config = function()
+      vim.cmd 'colorscheme onedark'
+    end
+  }
 
--- Theme
+  -- Colorizer
+  use {
+    'norcalli/nvim-colorizer.lua',
+    config = function()
+      require('colorizer').setup()
+    end
+  }
 
-cmd 'autocmd ColorScheme * highlight Normal ctermbg=NONE guibg=NONE'
-cmd 'colorscheme onedark'
+  -- Star search on visual selection
+  use {
+    'bronson/vim-visual-star-search'
+  }
 
--- Buffer line
+  -- Tmux navigator
+  use {
+    'christoomey/vim-tmux-navigator',
+    config = function()
+      vim.g.tmux_navigator_no_mappings = 1
 
-g.bufferline = {
-    animation = false,
-    icons = false,
-    maximum_padding = 0
-}
+      vim.api.nvim_set_keymap('n', '<M-h>', ':TmuxNavigateLeft<CR>',  {noremap = true, silent = true})
+      vim.api.nvim_set_keymap('n', '<M-j>', ':TmuxNavigateDown<CR>',  {noremap = true, silent = true})
+      vim.api.nvim_set_keymap('n', '<M-k>', ':TmuxNavigateUp<CR>',    {noremap = true, silent = true})
+      vim.api.nvim_set_keymap('n', '<M-l>', ':TmuxNavigateRight<CR>', {noremap = true, silent = true})
+    end
+  }
 
-map('n', '<LEADER><TAB>', ':BufferPick<CR>', {noremap = true, silent = true})
+  -- Restore cursor position
+  use 'farmergreg/vim-lastplace'
 
--- Color highlighter
-
-require('colorizer').setup()
-
--- File explorer
-
-require('nnn').setup{
-    set_default_mappings = false,
-    session = 'local',
-    action = {
-        ['<C-t>'] = 'tab split',
-        ['<C-v>'] = 'vsplit',
-        ['<C-x>'] = 'split'
+  -- Markdown preview
+  use {
+    'iamcco/markdown-preview.nvim',
+    run = function()
+      vim.fn['mkdp#util#install'](0)
+    end,
+    ft = {
+      'markdown'
     }
-}
+  }
 
-map('n', '<LEADER>n', ':NnnPicker<CR>',   {noremap = true})
-map('n', '<LEADER>N', ':NnnPicker %<CR>', {noremap = true})
+  -- Auto pair
+  use 'jiangmiao/auto-pairs'
 
--- Fuzzy finder
+  -- Comment
+  use 'tpope/vim-commentary'
 
-g.fzf_buffers_jump = 1
+  -- Auto hide search highlight
+  use 'romainl/vim-cool'
 
-map('n', '<LEADER>b', ':Buffers!<CR>', {noremap = true})
-map('n', '<LEADER>f', ':Files!<CR>\'', {noremap = true})
-map('n', '<LEADER>/', ':Rg!<CR>',      {noremap = true})
+  -- Fuzzy search
+  use {
+    'junegunn/fzf.vim',
+    requires = {
+      'junegunn/fzf'
+    },
+    config = function()
+      vim.g.fzf_buffers_jump = 1
+      vim.api.nvim_set_keymap('n', '<LEADER>b',  ':Buffers!<CR>', {noremap = true})
+      vim.api.nvim_set_keymap('n', '<LEADER>f',  ':Files!<CR>\'', {noremap = true})
+      vim.api.nvim_set_keymap('n', '<LEADER>/',  ':Rg!<CR>',      {noremap = true})
+      vim.api.nvim_set_keymap('n', '<LEADER>gt', ':GFiles!<CR>',  {noremap = true})
+    end
+  }
 
--- Git
+  -- File explorer
+  use {
+    'mcchrish/nnn.vim',
+    config = function()
+      require('nnn').setup({
+        set_default_mappings = false,
+        session = 'local',
+        -- TODO bug
+        -- action = {
+        --   ['<C-t>'] = 'tab split',
+        --   ['<C-v>'] = 'vsplit',
+        --   ['<C-x>'] = 'split'
+        -- }
+      })
+      -- vim.api.nvim_set_keypmap('n', '<LEADER>n', ':NnnPicker<CR>',   {noremap = true})
+      -- vim.api.nvim_set_keypmap('n', '<LEADER>N', ':NnnPicker %<CR>', {noremap = true})
+    end
+  }
 
-require('gitsigns').setup()
+  -- Show indent line
+  use {
+    "lukas-reineke/indent-blankline.nvim",
+    config = function()
+      vim.g.indent_blankline_show_current_context = true
+    end
+  }
 
-g.blamer_enabled = 1
-g.blamer_show_in_visual_modes = 0
-g.blamer_relative_time = 1
+  -- Syntax highlighting and objects
+  use {
+    'nvim-treesitter/nvim-treesitter',
+    config = function()
+      require'nvim-treesitter.configs'.setup {
+        ensure_installed = "maintained",
+        highlight = {
+          enable = true
+        }
+      }
+    end
+  }
 
-map('n', '<LEADER>gg', ':Git<SPACE>',      {noremap = true})
-map('n', '<LEADER>gs', ':Git<CR>',         {noremap = true})
-map('n', '<LEADER>gb', ':Git blame<CR>',   {noremap = true})
-map('n', '<LEADER>gc', ':Git commit<CR>',  {noremap = true})
-map('n', '<LEADER>gd', ':Gvdiffsplit<CR>', {noremap = true})
-map('n', '<LEADER>gl', ':Gclog<CR>',       {noremap = true})
-map('n', '<LEADER>gp', ':Git push<CR>',    {noremap = true})
-map('n', '<LEADER>gt', ':GFiles!<CR>',     {noremap = true})
-
--- Hop
-
-require('hop').setup({})
-
-map('n', 's', ":lua require'hop'.hint_words()<CR>", {})
-map('v', 's', ":lua require'hop'.hint_words()<CR>", {})
-
--- Language support
-
--- TODO enable treesitter when it's stable
--- require'nvim-treesitter.configs'.setup {
---     ensure_installed = "maintained", -- one of "all", "maintained" (parsers with maintainers), or a list of languages
---     highlight = {
---         enable = true
---     }
--- }
-
-require('lspinstall').setup()
-local servers = require('lspinstall').installed_servers()
-for _, server in pairs(servers) do
-    require('lspconfig')[server].setup({})
+  -- Buffer line
+  use {
+    'romgrk/barbar.nvim',
+    requires = {
+      'kyazdani42/nvim-web-devicons'
+    },
+    config = function()
+      vim.g.bufferline = {
+        animation = false,
+        maximum_padding = 0
+      }
+    end
+  }
 end
-
-require('compe').setup({
-    enabled = true;
-    autocomplete = true;
-
-    source = {
-        buffer = true;
-        nvim_lsp = true;
-        path = true;
-        vsnip = true;
-    };
 })
-
--- TODO clean up tab complete
-
-local t = function(str)
-    return vim.api.nvim_replace_termcodes(str, true, true, true)
-end
-
-local check_back_space = function()
-    local col = vim.fn.col(".") - 1
-    if col == 0 or vim.fn.getline("."):sub(col, col):match("%s") then
-        return true
-    else
-        return false
-    end
-end
-
-_G.tab_complete = function()
-    if vim.fn.call("vsnip#jumpable", {1}) == 1 then
-        return t "<Plug>(vsnip-jump-next)"
-    elseif vim.fn.pumvisible() == 1 then
-        return t "<C-n>"
-    elseif check_back_space() then
-        return t "<Tab>"
-    else
-        return vim.fn["compe#complete"]()
-    end
-end
-_G.s_tab_complete = function()
-    if vim.fn.call("vsnip#jumpable", {-1}) == 1 then
-        return t "<Plug>(vsnip-jump-prev)"
-    elseif vim.fn.pumvisible() == 1 then
-        return t "<C-p>"
-    else
-        return t "<S-Tab>"
-    end
-end
-
-map("i", "<CR>", "compe#confirm('<CR>')",     {expr = true})
-map("i", "<Tab>", "v:lua.tab_complete()",     {expr = true})
-map("s", "<Tab>", "v:lua.tab_complete()",     {expr = true})
-map("i", "<S-Tab>", "v:lua.s_tab_complete()", {expr = true})
-map("s", "<S-Tab>", "v:lua.s_tab_complete()", {expr = true})
-
--- Status line
-
-require('lualine').setup({
-    options = {
-        theme = 'onedark'
-    }
-})
-
--- Tmux navigator
-
-g.tmux_navigator_no_mappings = 1
-
-map('n', '<M-h>', ':TmuxNavigateLeft<CR>',  {noremap = true, silent = true})
-map('n', '<M-j>', ':TmuxNavigateDown<CR>',  {noremap = true, silent = true})
-map('n', '<M-k>', ':TmuxNavigateUp<CR>',    {noremap = true, silent = true})
-map('n', '<M-l>', ':TmuxNavigateRight<CR>', {noremap = true, silent = true})

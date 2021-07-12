482ec255531e05ef9104125996af20b813f7990a
Khue Doan <khuedoan98@gmail.com>
2021-07-13 00:25:18 +0700

    Use ; and , to trigger lightspeed f and t repeat

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -108,7 +108,20 @@ return require('packer').startup({function(use)
   }
 
   -- Sneak motion
-  use 'ggandor/lightspeed.nvim'
+  use {
+    'ggandor/lightspeed.nvim',
+    config = function ()
+      function repeat_ft(reverse)
+        local ls = require'lightspeed'
+        ls.ft['instant-repeat?'] = true
+        ls.ft:to(reverse, ls.ft['prev-t-like?'])
+      end
+      vim.api.nvim_set_keymap('n', ';', '<cmd>lua repeat_ft(false)<cr>', {noremap = true, silent = true})
+      vim.api.nvim_set_keymap('x', ';', '<cmd>lua repeat_ft(false)<cr>', {noremap = true, silent = true})
+      vim.api.nvim_set_keymap('n', ',', '<cmd>lua repeat_ft(true)<cr>', {noremap = true, silent = true})
+      vim.api.nvim_set_keymap('x', ',', '<cmd>lua repeat_ft(true)<cr>', {noremap = true, silent = true})
+    end
+  }
 
   -- Theme
   use {

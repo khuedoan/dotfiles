d7c46b55bc1020bd563b586b94cc5bb950f5a83a
Khue Doan <kdoan@axon.com>
2021-09-20 11:31:07 +0700

    fix(nvim): better custom keys for instant-repeat

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -111,15 +111,8 @@ return require('packer').startup({function(use)
   use {
     'ggandor/lightspeed.nvim',
     config = function ()
-      function repeat_ft(reverse)
-        local ls = require'lightspeed'
-        ls.ft['instant-repeat?'] = true
-        ls.ft:to(reverse, ls.ft['prev-t-like?'])
-      end
-      vim.api.nvim_set_keymap('n', ';', '<cmd>lua repeat_ft(false)<cr>', {noremap = true, silent = true})
-      vim.api.nvim_set_keymap('x', ';', '<cmd>lua repeat_ft(false)<cr>', {noremap = true, silent = true})
-      vim.api.nvim_set_keymap('n', ',', '<cmd>lua repeat_ft(true)<cr>', {noremap = true, silent = true})
-      vim.api.nvim_set_keymap('x', ',', '<cmd>lua repeat_ft(true)<cr>', {noremap = true, silent = true})
+      require'lightspeed'.opts.instant_repeat_fwd_key = ';'
+      require'lightspeed'.opts.instant_repeat_bwd_key = ','
     end
   }
 

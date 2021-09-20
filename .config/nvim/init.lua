484892274f20ffc8fa35ec106eeee423d4dc3ab4
Khue Doan <kdoan@axon.com>
2021-09-21 00:49:06 +0700

    Revert "Remove save and quit with Ctrl"
    This reverts commit 379de2a73358c42cc2ac1e5a85fbaf9fc6cf0705.

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -34,6 +34,11 @@ vim.g.mapleader = ' '
 vim.api.nvim_set_keymap('v', '<C-c>', '"+y',    {noremap = true})
 vim.api.nvim_set_keymap('i', '<C-v>', '<C-r>+', {noremap = true})
 
+-- Save and quit
+
+vim.api.nvim_set_keymap('n', '<C-s>', ':write<CR>', {noremap = true})
+vim.api.nvim_set_keymap('n', '<C-q>', ':quit<CR>',  {noremap = true})
+
 -- Replace
 
 vim.api.nvim_set_keymap('n', '<LEADER>r', ':%s///g<LEFT><LEFT>',  {noremap = true})

01ab8d6c28a6d037658e417f7a8a3ac5e98b4670
Khue Doan <khuedoan98@gmail.com>
2021-09-26 14:36:43 +0700

    feat(nvim): expand full path in alternate fuzzy file search

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -200,7 +200,7 @@ return require'packer'.startup {
         vim.g.fzf_buffers_jump = 1
         vim.api.nvim_set_keymap('n', '<LEADER>bb', ':Buffers!<CR>', {noremap = true})
         vim.api.nvim_set_keymap('n', '<LEADER>f', ':Files!<CR>\'', {noremap = true})
-        vim.api.nvim_set_keymap('n', '<LEADER>F', ':Files! %:p:h<CR>\'', {noremap = true})
+        vim.api.nvim_set_keymap('n', '<LEADER>F', ':Files! '..vim.fn.expand('%:p:h'), {noremap = true})
         vim.api.nvim_set_keymap('n', '<LEADER>/', ':Rg!<CR>', {noremap = true})
         vim.api.nvim_set_keymap('n', '<LEADER>gt', ':GFiles!<CR>', {noremap = true})
       end

37c5cf77a3ce3addf1ea89e54ea2e43c1b75a0c2
Khue Doan <khuedoan98@gmail.com>
2021-10-07 20:17:11 +0700

    style(nvim): better Lua syntax for indent line config

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -229,9 +229,11 @@ return require'packer'.startup {
     use {
       "lukas-reineke/indent-blankline.nvim",
       config = function()
-        vim.g.indent_blankline_char = "▏"
-        vim.g.indent_blankline_show_trailing_blankline_indent = false
-        vim.g.indent_blankline_show_first_indent_level = false
+        require("indent_blankline").setup {
+          char = "▏",
+          show_first_indent_level = false,
+          show_trailing_blankline_indent = false,
+        }
       end
     }
 

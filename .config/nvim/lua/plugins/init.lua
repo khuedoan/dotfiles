73e367155789b1752b4612a5de2590b67d38494f
Khue Doan <mail@khuedoan.com>
2022-04-12 13:16:06 +0700

    fix(nvim): install all treesitter parsers
    "maintained" is deprecated.
    
    https://github.com/nvim-treesitter/nvim-treesitter/pull/2763

--- .config/nvim/lua/plugins/init.lua
+++ .config/nvim/lua/plugins/init.lua
@@ -236,7 +236,7 @@ return require'packer'.startup {
       run = ':TSUpdate',
       config = function()
         require'nvim-treesitter.configs'.setup {
-          ensure_installed = "maintained",
+          ensure_installed = "all",
           highlight = {
             enable = true
           }

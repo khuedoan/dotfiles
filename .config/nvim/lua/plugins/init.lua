c1a94c3ff666ff28214490567544e836a4a5020f
Khue Doan <mail@khuedoan.com>
2022-04-16 12:26:59 +0700

    refactor(nvim)!: replace fzf with telescope

--- .config/nvim/lua/plugins/init.lua
+++ .config/nvim/lua/plugins/init.lua
@@ -68,7 +68,7 @@ return require'packer'.startup {
           disable_context_highlighting = true
         }
 
-        vim.api.nvim_set_keymap('n', '<LEADER>gs', ':Neogit kind=split<CR>', {noremap = true})
+        vim.api.nvim_set_keymap('n', '<LEADER>gg', ':Neogit kind=split<CR>', {noremap = true})
       end
     }
 
@@ -171,22 +171,6 @@ return require'packer'.startup {
     -- Auto hide search highlight
     use {
       'romainl/vim-cool'
-    }
-
-    -- Fuzzy search
-    use {
-      'junegunn/fzf.vim',
-      requires = {
-        'junegunn/fzf'
-      },
-      config = function()
-        vim.g.fzf_buffers_jump = 1
-        vim.api.nvim_set_keymap('n', '<LEADER><TAB>', ':Buffers!<CR>', {noremap = true})
-        vim.api.nvim_set_keymap('n', '<LEADER>ff', ':Files!<CR>\'', {noremap = true})
-        vim.api.nvim_set_keymap('n', '<LEADER>f.', ':Files! '..vim.fn.expand('%:p:h'), {noremap = true})
-        vim.api.nvim_set_keymap('n', '<LEADER>f/', ':Rg!<CR>', {noremap = true})
-        vim.api.nvim_set_keymap('n', '<LEADER>fg', ':GFiles!<CR>', {noremap = true})
-      end
     }
 
     -- File explorer
@@ -470,6 +454,37 @@ return require'packer'.startup {
         'gS',
         'gJ'
       },
+    }
+
+    use {
+      'nvim-telescope/telescope.nvim',
+      requires = {
+        {'nvim-lua/plenary.nvim'},
+      }
+    }
+
+    use {
+      'nvim-telescope/telescope-fzf-native.nvim',
+      run = 'make',
+      config = function()
+        require('telescope').setup {
+          defaults = {
+          },
+        }
+        require('telescope').load_extension('fzf')
+        require'telescope.themes'.get_ivy()
+
+        vim.api.nvim_set_keymap("n", "<LEADER><LEADER>", ":Telescope find_files theme=get_ivy<CR>", { noremap = true })
+        vim.api.nvim_set_keymap("n", "<LEADER>ff", ":Telescope find_files theme=get_ivy<CR>", { noremap = true })
+
+        vim.api.nvim_set_keymap("n", "<LEADER>sp", ":Telescope live_grep theme=get_ivy<CR>", { noremap = true })
+        vim.api.nvim_set_keymap("n", "<LEADER>/", ":Telescope live_grep theme=get_ivy<CR>", { noremap = true })
+
+        vim.api.nvim_set_keymap("n", "<LEADER>bb", ":Telescope buffers theme=get_ivy<CR>", { noremap = true })
+        vim.api.nvim_set_keymap("n", "<LEADER>,", ":Telescope buffers theme=get_ivy<CR>", { noremap = true })
+
+        vim.api.nvim_set_keymap("n", "<LEADER>sb", ":Telescope current_buffer_fuzzy_find theme=get_ivy<CR>", { noremap = true })
+      end
     }
 
     if packer_bootstrap then

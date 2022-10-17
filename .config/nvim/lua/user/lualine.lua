978eae103a2fda20970c7235d95bbaa91cb32f75
Khue Doan <mail@khuedoan.com>
2022-10-17 11:30:38 +0700

    refactor(nvim): more minimal lualine and project

--- .config/nvim/lua/user/lualine.lua
+++ .config/nvim/lua/user/lualine.lua
@@ -3,52 +3,13 @@ if not status_ok then
     return
 end
 
-local hide_in_width = function()
-    return vim.fn.winwidth(0) > 80
-end
-
-local diagnostics = {
-    "diagnostics",
-    sources = { "nvim_diagnostic" },
-    sections = { "error", "warn" },
-    symbols = { error = " ", warn = " " },
-    colored = false,
-    always_visible = true,
-}
-
-local diff = {
-    "diff",
-    colored = false,
-    symbols = { added = "", modified = "", removed = "" }, -- changes diff symbols
-    cond = hide_in_width,
-}
-
-local filetype = {
-    "filetype",
-    icons_enabled = false,
-}
-
-local location = {
-    "location",
-    padding = 0,
-}
-
-local spaces = function()
-    return "spaces: " .. vim.api.nvim_buf_get_option(0, "shiftwidth")
-end
-
 lualine.setup({
     options = {
         globalstatus = true,
         theme = "auto",
-        disabled_filetypes = { "alpha", "dashboard" },
-    },
-    sections = {
-        lualine_a = { "mode" },
-        lualine_b = { "branch" },
-        lualine_c = { diagnostics },
-        lualine_x = { diff, spaces, "encoding", filetype },
-        lualine_y = { location },
-        lualine_z = { "progress" },
+        disabled_filetypes = {
+            "alpha",
+            "dashboard",
+        },
     },
 })
--- .config/nvim/lua/user/project.lua
+++ .config/nvim/lua/user/project.lua
@@ -3,12 +3,8 @@ if not status_ok then
     return
 end
 project.setup({
-
-    -- detection_methods = { "lsp", "pattern" }, -- NOTE: lsp detection will get annoying with multiple langs in one project
     detection_methods = { "pattern" },
-
-    -- patterns used to detect root dir, when **"pattern"** is in detection_methods
-    patterns = { ".git", "Makefile", "package.json" },
+    patterns = { ".git" },
 })
 
 local tele_status_ok, telescope = pcall(require, "telescope")

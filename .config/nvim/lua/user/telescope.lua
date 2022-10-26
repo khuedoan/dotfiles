a9c01df926e6c9c4407601cf2f769f67f63b779d
Khue Doan <mail@khuedoan.com>
2022-10-26 22:27:34 +0700

    refactor(nvim): change telescope path display

--- .config/nvim/lua/user/telescope.lua
+++ .config/nvim/lua/user/telescope.lua
@@ -7,13 +7,15 @@ local actions = require("telescope.actions")
 
 telescope.setup({
     defaults = {
-        path_display = { "smart" },
+        path_display = { "absolute" },
         file_ignore_patterns = { ".git/", "node_modules" },
 
         mappings = {
             i = {
                 ["<Down>"] = actions.cycle_history_next,
                 ["<Up>"] = actions.cycle_history_prev,
+                ["<C-n>"] = actions.cycle_history_next,
+                ["<C-p>"] = actions.cycle_history_prev,
                 ["<C-j>"] = actions.move_selection_next,
                 ["<C-k>"] = actions.move_selection_previous,
             },

7ee3dedaba7f212699c7b121c7d5ab9bacf22498
Khue Doan <mail@khuedoan.com>
2022-11-29 23:55:32 +0700

    refactor(nvim): use float toggle terminal

--- .config/nvim/lua/user/toggleterm.lua
+++ .config/nvim/lua/user/toggleterm.lua
@@ -12,7 +12,7 @@ toggleterm.setup({
     start_in_insert = true,
     insert_mappings = true,
     persist_size = true,
-    direction = "horizontal",
+    direction = "float",
     close_on_exit = true,
     shell = vim.o.shell,
     float_opts = {

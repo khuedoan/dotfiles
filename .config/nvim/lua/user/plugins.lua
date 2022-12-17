bcbe34fab2161d0fcc14f7ebadb7c16bcd320ead
Khue Doan <mail@khuedoan.com>
2022-12-18 04:33:55 +0700

    style(nvim): change Telescope theme

--- .config/nvim/lua/user/plugins.lua
+++ .config/nvim/lua/user/plugins.lua
@@ -116,7 +116,48 @@ return packer.startup(function(use)
     use({ "https://github.com/jose-elias-alvarez/null-ls.nvim" }) -- for formatters and linters
 
     -- Telescope
-    use({ "https://github.com/nvim-telescope/telescope.nvim" })
+    use({
+        "https://github.com/nvim-telescope/telescope.nvim",
+        config = function()
+            local telescope = require("telescope")
+            local actions = require("telescope.actions")
+            local themes = require("telescope.themes")
+            telescope.setup({
+                defaults = themes.get_ivy {
+                    path_display = { "absolute" },
+                    file_ignore_patterns = { ".git/", "node_modules" },
+
+                    mappings = {
+                        i = {
+                            ["<Down>"] = actions.cycle_history_next,
+                            ["<Up>"] = actions.cycle_history_prev,
+                            ["<C-n>"] = actions.cycle_history_next,
+                            ["<C-p>"] = actions.cycle_history_prev,
+                            ["<C-j>"] = actions.move_selection_next,
+                            ["<C-k>"] = actions.move_selection_previous,
+                        },
+                    },
+                },
+                extensions = {
+                    fzf = {
+                        fuzzy = true, -- false will only do exact matching
+                        override_generic_sorter = true, -- override the generic sorter
+                        override_file_sorter = true, -- override the file sorter
+                        case_mode = "smart_case", -- or "ignore_case" or "respect_case"
+                        -- the default case_mode is "smart_case"
+                    },
+                },
+                pickers = {
+                    find_files = {
+                        no_ignore = true,
+                        no_ignore_parent = true,
+                    },
+                },
+            })
+
+            telescope.load_extension("fzf")
+        end
+    })
     use({
         "https://github.com/nvim-telescope/telescope-fzf-native.nvim",
         run = "make",

a28a1ae9a76e7ec544128b0b78aa5463598bc094
Khue Doan <mail@khuedoan.com>
2023-01-17 00:42:52 +0700

    chore(nvim): add nvim-tree back

--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -191,6 +191,18 @@ return require("packer").startup(function(use)
             })
         end,
     })
+
+    use({
+        "https://github.com/nvim-tree/nvim-tree.lua",
+        config = function()
+            require("nvim-tree").setup({
+                update_focused_file = {
+                    enable = true,
+                    update_root = true,
+                },
+            })
+        end
+    })
     -- }}}
 
     -- {{{ IntelliSense

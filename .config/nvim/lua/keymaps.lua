ac246f71a36becb195999be5cd111e1b312806c4
Khue Doan <mail@khuedoan.com>
2023-06-03 13:45:01 +0700

    refactor(nvim): move leader keymaps to keymaps.lua
    With lazy.nvim's lazy loading, the plugin will be automatically
    loaded when it is required in keymaps.lua

--- .config/nvim/lua/keymaps.lua
+++ .config/nvim/lua/keymaps.lua
@@ -1,10 +1,96 @@
--- --Remap space as leader key
+-- Remap space as leader key
 vim.keymap.set("", "<Space>", "<Nop>", { silent = true })
 vim.g.mapleader = " "
 
--- Save and quit
-vim.keymap.set("n", "<C-s>", ":update<cr>", { silent = true })
-vim.keymap.set("n", "<C-q>", ":quit<cr>", { silent = true })
+local leader_keymaps = {
+    b = {
+        name = "buffer",
+        b = { ":Buffers!<cr>", "Switch buffer" },
+        n = { ":bnext<cr>", "Next buffer" },
+        p = { ":bprevious<cr>", "Previous buffer" },
+        d = {
+            function()
+                require("mini.bufremove").delete(0, false)
+            end,
+            "Delete buffer",
+        },
+        l = { ":b#<cr>", "Switch to last buffer" },
+    },
+    f = {
+        name = "file",
+        f = { ":Files!<cr>", "Find file" },
+        F = { ":Files! " .. vim.fn.expand("%:p:h") .. "<cr>", "Find file from here" },
+        g = { ":GitFiles!<cr>", "Find file in git project" },
+        y = {
+            function()
+                local path = vim.fn.expand("%:p:~")
+                vim.fn.setreg("+", path)
+                print("Copied path: " .. path)
+            end,
+            "Yank file path",
+        },
+        Y = {
+            function()
+                local path = vim.fn.expand("%")
+                vim.fn.setreg("+", path)
+                print("Copied path: " .. path)
+            end,
+            "Yank file path from project",
+        },
+    },
+    p = {
+        name = "project",
+        p = { ":Telescope projects<cr>", "Switch project" },
+        b = { ":NnnPicker<cr>", "Browse project" },
+        B = { ":NnnPicker %:p:h<cr>", "Browse project from here" },
+    },
+    s = {
+        name = "search",
+        p = { ":Rg!<cr>", "Search project" },
+        r = {
+            function()
+                require("spectre").open_file_search()
+            end,
+            "Search and replace",
+        },
+    },
+    g = {
+        name = "git",
+        g = { ":Neogit<cr>", "Git status" },
+        s = { ":Neogit<cr>", "Git status" },
+        i = { ":Octo issue list<cr>", "GitHub issues" },
+        p = { ":Octo pr list<cr>", "GitHub pull requests" },
+    },
+    m = {
+        name = "markdown",
+        p = { ":MarkdownPreview<cr>", "Markdown preview" },
+    },
+    o = {
+        name = "open",
+        t = { ":edit ~/Documents/notes/todo.md<cr>", "Todo list" },
+        p = { ":NeoTreeRevealToggle<cr>", "Project sidebar" },
+    },
+    [":"] = { ":Legendary<cr>", "Commands" },
+}
 
--- Copy to system clipboard
-vim.keymap.set("v", "<C-c>", '"+y', { silent = true })
+-- Aliases
+leader_keymaps["<leader>"] = leader_keymaps.f.f
+leader_keymaps["/"] = leader_keymaps.s.p
+leader_keymaps[","] = leader_keymaps.b.b
+leader_keymaps["<"] = leader_keymaps.b.B
+leader_keymaps["`"] = leader_keymaps.b.l
+
+require("which-key").register(leader_keymaps, { prefix = "<leader>" })
+
+require("which-key").register({
+    ["<C-s>"] = { ":update<cr>", "Save" },
+    ["<C-q>"] = { ":quit<cr>", "Quit" },
+}, {
+    mode = "n",
+})
+
+require("which-key").register({
+    ["<C-c>"] = { '"+y', "Copy to system clipboard" },
+}, {
+    mode = "v",
+})

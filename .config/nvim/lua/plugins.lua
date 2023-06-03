a9010a28eda770057bfcf14183429a6516589e4a
Khue Doan <mail@khuedoan.com>
2023-06-04 03:08:22 +0700

    fix(nvim): remove Grammarly LSP
    Conflicts with oil.nvim `-` keymap

--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -141,7 +141,6 @@ return require("lazy").setup({
                     "cssls",
                     "dockerls",
                     "gopls",
-                    "grammarly",
                     "html",
                     "jsonls",
                     "lua_ls",

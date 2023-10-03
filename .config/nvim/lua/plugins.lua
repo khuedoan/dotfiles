8a0d8249a7d20055894b7ef0055eceba97b85540
Khue Doan <mail@khuedoan.com>
2023-10-03 13:37:17 +0700

    feat(nvim): add surround plugin

--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -434,6 +434,26 @@ return require("lazy").setup({
             })
         end,
     },
+
+    {
+        "https://github.com/echasnovski/mini.surround",
+        config = function ()
+            require("mini.surround").setup({
+                mappings = {
+                    add = "gza",
+                    delete = "gzd",
+                    find = "gzf",
+                    find_left = "gzF",
+                    highlight = "gzh",
+                    replace = "gzr",
+                    update_n_lines = "gzn",
+                },
+            })
+        end,
+        keys = {
+            { "gz", desc = "Surround" },
+        },
+    },
     -- }}}
 
     -- {{{ Keymaps

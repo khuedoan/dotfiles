f23ab5be41d59634d1e6ea1600a10f3361da5475
Khue Doan <mail@khuedoan.com>
2023-10-13 23:05:50 +0700

    style(nvim): minor status bar adjust
    And format

--- .config/nvim/lua/options.lua
+++ .config/nvim/lua/options.lua
@@ -1,20 +1,19 @@
-vim.opt.completeopt = { "menuone", "noselect" } -- mostly just for cmp
-vim.opt.ignorecase = true -- ignore case in search patterns
-vim.opt.smartcase = true -- smart case
-vim.opt.smartindent = true -- make indenting smarter again
-vim.opt.splitbelow = true -- force all horizontal splits to go below current window
-vim.opt.splitright = true -- force all vertical splits to go to the right of current window
-vim.opt.swapfile = false -- creates a swapfile
-vim.opt.undofile = true -- enable persistent undo
-vim.opt.expandtab = true -- convert tabs to spaces
-vim.opt.shiftwidth = 4 -- the number of spaces inserted for each indentation
-vim.opt.tabstop = 4 -- insert 2 spaces for a tab
-vim.opt.cursorline = true -- highlight the current line
-vim.opt.number = true -- set numbered lines
+vim.opt.ignorecase = true     -- ignore case in search patterns
+vim.opt.smartcase = true      -- smart case
+vim.opt.smartindent = true    -- make indenting smarter again
+vim.opt.splitbelow = true     -- force all horizontal splits to go below current window
+vim.opt.splitright = true     -- force all vertical splits to go to the right of current window
+vim.opt.swapfile = false      -- creates a swapfile
+vim.opt.undofile = true       -- enable persistent undo
+vim.opt.expandtab = true      -- convert tabs to spaces
+vim.opt.shiftwidth = 4        -- the number of spaces inserted for each indentation
+vim.opt.tabstop = 4           -- insert 2 spaces for a tab
+vim.opt.cursorline = true     -- highlight the current line
+vim.opt.number = true         -- set numbered lines
 vim.opt.relativenumber = true -- show relative line number
-vim.opt.signcolumn = "yes" -- always show the sign column, otherwise it would shift the text each time
-vim.opt.scrolloff = 3 -- minimal number of screen lines to keep above and below the cursor
-vim.opt.sidescrolloff = 3 -- minimal number of screen columns to keep to the left and right of the cursor if wrap is `false`
+vim.opt.signcolumn = "yes"    -- always show the sign column, otherwise it would shift the text each time
+vim.opt.scrolloff = 3         -- minimal number of screen lines to keep above and below the cursor
+vim.opt.sidescrolloff = 3     -- minimal number of screen columns to keep to the left and right of the cursor if wrap is `false`
 
 -- Custom statusline
 vim.opt.laststatus = 3      -- global statusline
@@ -24,4 +23,5 @@ vim.opt.statusline =
     "%m" ..                 -- modifed
     "%r" ..                 -- readonly
     "%=" ..                 -- separator
-    " %l:%c/%L "            -- current line and column per total lines
+    "%l:%c / %L" ..         -- current line and column per total lines
+    " "                     -- padding

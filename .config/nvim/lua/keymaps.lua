cdd35663640c457a727a8f4b310185113c554089
Khue Doan <mail@khuedoan.com>
2024-06-13 10:27:43 +0700

    refactor(nvim): swap search and replace keymaps

--- .config/nvim/lua/keymaps.lua
+++ .config/nvim/lua/keymaps.lua
@@ -39,8 +39,17 @@ local keymaps = {
             l = { "<Cmd>FzfLua blines<CR>", "Search lines in current file" },
             p = { "<Cmd>FzfLua grep_project<CR>", "Search project (fuzzy)" },
             P = { "<Cmd>FzfLua live_grep<CR>", "Search project (regex)" },
-            r = { function() require("grug-far").grug_far({}) end, "Search and replace" },
-            R = { function() require("grug-far").grug_far({ prefills = { flags = vim.fn.expand("%") } }) end, "Search and replace in current file" },
+            r = {
+                function()
+                    require("grug-far").grug_far({
+                        prefills = {
+                            flags = vim.fn.expand("%"),
+                        },
+                    })
+                end,
+                "Search and replace in current file",
+            },
+            R = { function() require("grug-far").grug_far({}) end, "Search and replace in project" },
         },
         g = {
             name = "git",

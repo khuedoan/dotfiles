d3adba1873b5aa869ff9e1c9554bfd87300f7196
Khue Doan <mail@khuedoan.com>
2025-05-19 23:57:09 +0700

    feat(emacs): add fzf

---
+++ .config/emacs/early-init.el
@@ -0,0 +1,4 @@
+(tool-bar-mode -1)
+(menu-bar-mode -1)
+(scroll-bar-mode -1)
+(setq use-dialog-box nil)
--- .config/emacs/init.el
+++ .config/emacs/init.el
@@ -1,13 +1,10 @@
-(tool-bar-mode -1)
-(menu-bar-mode -1)
-(scroll-bar-mode -1)
-(setq use-dialog-box nil)
+(setq display-line-numbers-type 'relative)
+(global-display-line-numbers-mode 1)
 
 (require 'package)
 (setq package-archives
       '(("melpa" . "https://melpa.org/packages/")
-        ("elpa"  . "https://elpa.gnu.org/packages/")))
-(package-initialize)
+        ("elpa"  . "https://stable.elpa.gnu.org/packages/")))
 
 (unless (package-installed-p 'use-package)
   (package-refresh-contents)
@@ -16,7 +13,6 @@
 (setq use-package-always-ensure t)
 
 (use-package doom-themes
-  :ensure t
   :config
   (load-theme 'doom-one t)
   (doom-themes-visual-bell-config)
@@ -26,3 +22,13 @@
   :init
   :config
   (evil-mode 1))
+
+(use-package fzf
+  :config
+  (setq fzf/args "--layout=default -x --color bw --print-query --margin=1,0 --no-hscroll"))
+
+(evil-define-key 'normal 'global
+  (kbd "SPC SPC") 'fzf-find-file
+  (kbd "SPC ,") 'fzf-switch-buffer
+  (kbd "SPC /") 'fzf-grep-project
+  (kbd "SPC fg") 'fzf-git-files)

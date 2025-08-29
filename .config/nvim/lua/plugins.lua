4845cef8d58b65e4bdaf5388975ac04a1bb5730d
Khue Doan <mail@khuedoan.com>
2025-08-29 10:57:43 +0700

    Revert "refactor(nvim): replace frecency with combined search"
    This reverts commit 21cef594a14dc9c1c6f9d98854ed3754a35ffae1.
    
    Having separate keybind feels more natural.

--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -41,6 +41,9 @@ return require("lazy").setup({
     -- {{{ Search
     {
         "https://github.com/ibhagwan/fzf-lua",
+        dependencies = {
+            "https://github.com/elanmed/fzf-lua-frecency.nvim",
+        },
         event = "VeryLazy",
         config = function()
             require("fzf-lua").setup({
@@ -52,6 +55,9 @@ return require("lazy").setup({
                 },
             })
             require("fzf-lua").register_ui_select()
+            require("fzf-lua-frecency").setup({
+                display_score = false,
+            })
         end,
     },
 

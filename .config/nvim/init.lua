96e0fe1be82c4f688f5e5e5183c69282f34cdc6e
Khue Doan <mail@khuedoan.com>
2026-04-05 08:39:25 +0700

    style(nvim): adjust comments

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,31 +1,28 @@
 vim.loader.enable() -- fast load dark magic
-
-vim.opt.ignorecase = true     -- ignore case in search patterns
-vim.opt.smartcase = true      -- smart case
-vim.opt.smartindent = true    -- make indenting smarter again
-vim.opt.splitbelow = true     -- force all horizontal splits to go below current window
-vim.opt.splitright = true     -- force all vertical splits to go to the right of current window
-vim.opt.swapfile = false      -- creates a swapfile
-vim.opt.undofile = true       -- enable persistent undo
-vim.opt.expandtab = true      -- convert tabs to spaces
-vim.opt.shiftwidth = 4        -- the number of spaces inserted for each indentation
-vim.opt.tabstop = 4           -- insert 4 spaces for a tab
-vim.opt.cursorline = true     -- highlight the current line
-vim.opt.number = true         -- set numbered lines
+vim.opt.ignorecase = true -- ignore case in search patterns
+vim.opt.smartcase = true -- smart case
+vim.opt.smartindent = true -- make indenting smarter again
+vim.opt.splitbelow = true -- force all horizontal splits to go below current window
+vim.opt.splitright = true -- force all vertical splits to go to the right of current window
+vim.opt.swapfile = false -- creates a swapfile
+vim.opt.undofile = true -- enable persistent undo
+vim.opt.expandtab = true -- convert tabs to spaces
+vim.opt.shiftwidth = 4 -- the number of spaces inserted for each indentation
+vim.opt.tabstop = 4 -- insert 4 spaces for a tab
+vim.opt.cursorline = true -- highlight the current line
+vim.opt.number = true -- set numbered lines
 vim.opt.relativenumber = true -- show relative line number
-vim.opt.signcolumn = "yes"    -- always show the sign column, otherwise it would shift the text each time
-vim.opt.scrolloff = 3         -- minimal number of screen lines to keep above and below the cursor
-vim.opt.laststatus = 0        -- disable statusline
+vim.opt.signcolumn = "yes" -- always show the sign column, otherwise it would shift the text each time
+vim.opt.scrolloff = 3 -- minimal number of screen lines to keep above and below the cursor
+vim.opt.laststatus = 0 -- disable statusline
 
-vim.pack.add({
-    "https://github.com/navarasu/onedark.nvim",
-})
-
+vim.pack.add({"https://github.com/navarasu/onedark.nvim"})
 require("onedark").setup({
     transparent = true,
 })
 require("onedark").load()
 
+-- Lazy load
 vim.schedule(function()
     vim.g.mapleader = " "
     local map = vim.keymap.set

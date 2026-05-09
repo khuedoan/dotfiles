2837ff17276496d0a193966b06beb6d19de56b8e
Khue Doan <mail@khuedoan.com>
2026-05-09 11:19:05 +0700

    refactor(dotfiles): remove legacy repo clone

--- modules/dotfiles/default.nix
+++ modules/dotfiles/default.nix
@@ -22,22 +22,5 @@ in
         sha256 = "1g120j4z6665j4wh2g84m4rb24gvzdxyhx9lqym68cwn8ny2j7fz";
       };
     };
-
-    home.activation.dotfiles = ''
-      set -eu
-
-      if [ ! -d "$HOME/.git" ]; then
-        ${pkgs.git}/bin/git init
-        ${pkgs.git}/bin/git config status.showUntrackedFiles no
-        ${pkgs.git}/bin/git remote add origin https://github.com/khuedoan/dotfiles
-
-        until ${pkgs.curl}/bin/curl --fail --silent --head --output /dev/null https://github.com; do
-          sleep 1
-        done
-
-        ${pkgs.git}/bin/git pull origin master
-        ${pkgs.git}/bin/git branch --set-upstream-to=origin/master master
-      fi
-    '';
   };
 }

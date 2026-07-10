ded6c126baa87077b2b8b36e5713549172a7dfa4
Khue Doan <mail@khuedoan.com>
2026-07-10 20:24:05 +0700

    style(emacs): change window title

--- modules/dotfiles/home/.config/doom/config.el
+++ modules/dotfiles/home/.config/doom/config.el
@@ -3,6 +3,8 @@
 ;; Place your private configuration here! Remember, you do not need to run 'doom
 ;; sync' after modifying this file!
 
+(setq frame-title-format '("Emacs - %b")
+      icon-title-format frame-title-format)
 
 ;; Some functionality uses this to identify you, e.g. GPG configuration, email
 ;; clients, file templates and snippets. It is optional.

4a48d0a270e3c5ae1bf23763510f20ec5b14a45c
Khue Doan <mail@khuedoan.com>
2026-07-19 15:21:18 +0700

    feat: OS-agnostic notes sync

--- modules/personal/default.nix
+++ modules/personal/default.nix
@@ -1,7 +1,24 @@
-{ platform, ... }:
+{ config, platform, ... }:
+
+let
+  username = config.primaryUser.username;
+in
 
 {
   imports = [
     ./${platform.parsed.kernel.name}.nix
   ];
+
+  home-manager.users.${username} =
+    { config, ... }:
+    {
+      services.git-sync = {
+        enable = true;
+        repositories.notes = {
+          path = "${config.home.homeDirectory}/Projects/notes";
+          uri = "gh:khuedoan/notes.git";
+          interval = 300;
+        };
+      };
+    };
 }
--- modules/personal/linux.nix
+++ modules/personal/linux.nix
@@ -5,48 +5,7 @@ let
 in
 
 {
-  home-manager.users.${username} = {
-    home.packages = with pkgs.unstable; [
-      signal-desktop
-    ];
-
-    systemd.user = {
-      services.sync-notes = {
-        Unit.Description = "Sync notes";
-        Service = {
-          Type = "oneshot";
-          WorkingDirectory = "%h/Projects/notes";
-          Environment = [
-            "HOSTNAME=%H"
-            "GIT_SSH_COMMAND=/run/current-system/sw/bin/ssh"
-          ];
-          ExecStart = "${pkgs.writeTextFile {
-            name = "sync-notes";
-            executable = true;
-            text = ''
-              #!${pkgs.bash}/bin/sh
-              set -eu
-
-              if [ -n "$(${pkgs.git}/bin/git status --porcelain)" ]; then
-                ${pkgs.git}/bin/git add --all
-                if ! ${pkgs.git}/bin/git diff --cached --quiet; then
-                  ${pkgs.git}/bin/git commit -m "Update notes from $HOSTNAME"
-                fi
-              fi
-              ${pkgs.git}/bin/git pull --rebase --strategy-option theirs
-              ${pkgs.git}/bin/git push
-            '';
-          }}";
-        };
-      };
-      timers.sync-notes = {
-        Unit.Description = "Sync notes every 5 minutes";
-        Timer = {
-          OnUnitActiveSec = "5min";
-          Persistent = true;
-        };
-        Install.WantedBy = [ "timers.target" ];
-      };
-    };
-  };
+  home-manager.users.${username}.home.packages = with pkgs.unstable; [
+    signal-desktop
+  ];
 }

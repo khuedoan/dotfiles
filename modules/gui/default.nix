3752c56a1b83f1e4f2a1399d7b07eb8a30b5d08f
Khue Doan <mail@khuedoan.com>
2026-07-27 14:03:08 +0700

    refactor: move notes sync to GUI
    Technically it's not GUI but I only write notes on workstation/laptop.

--- modules/gui/default.nix
+++ modules/gui/default.nix
@@ -1,4 +1,4 @@
-{ pkgs, platform, ... }:
+{ config, pkgs, platform, ... }:
 
 {
   imports = [
@@ -9,4 +9,17 @@
     nerd-fonts.fira-code
     nerd-fonts.symbols-only
   ];
+
+  home-manager.users.${config.primaryUser.username} =
+    { config, ... }:
+    {
+      services.git-sync = {
+        enable = true;
+        repositories.notes = {
+          path = "${config.home.homeDirectory}/Projects/notes";
+          uri = "git@github.com:khuedoan/notes.git";
+          interval = 300;
+        };
+      };
+    };
 }
--- modules/personal/default.nix
+++ modules/personal/default.nix
@@ -1,24 +1,7 @@
-{ config, platform, ... }:
-
-let
-  username = config.primaryUser.username;
-in
+{ platform, ... }:
 
 {
   imports = [
     ./${platform.parsed.kernel.name}.nix
   ];
-
-  home-manager.users.${username} =
-    { config, ... }:
-    {
-      services.git-sync = {
-        enable = true;
-        repositories.notes = {
-          path = "${config.home.homeDirectory}/Projects/notes";
-          uri = "gh:khuedoan/notes.git";
-          interval = 300;
-        };
-      };
-    };
 }

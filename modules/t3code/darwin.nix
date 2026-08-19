41a642578d968a8e03eb5696e9f6b59e0aead47a
Khue Doan <mail@khuedoan.com>
2026-08-19 15:20:24 +0700

    fix(t3code): use user service on macOS

--- modules/t3code/darwin.nix
+++ modules/t3code/darwin.nix
@@ -5,22 +5,34 @@
   ...
 }:
 
-let
-  username = config.primaryUser.username;
-  home = config.users.users.${username}.home;
-in
 {
-  launchd.daemons.t3code.serviceConfig = {
-    EnvironmentVariables.HOME = home;
-    KeepAlive = true;
-    ProgramArguments = [
-      (lib.getExe pkgs.unofficial.t3code)
-      "serve"
-      "--host"
-      "0.0.0.0"
-    ];
-    RunAtLoad = true;
-    UserName = username;
-    WorkingDirectory = home;
-  };
+  home-manager.users.${config.primaryUser.username} =
+    { config, ... }:
+    {
+      launchd.agents.t3code = {
+        enable = true;
+        config = {
+          EnvironmentVariables = {
+            # launchd has minimal PATH by default
+            PATH =
+              lib.replaceStrings
+                [ "$HOME" "$USER" ]
+                [
+                  config.home.homeDirectory
+                  config.home.username
+                ]
+                config.environment.systemPath;
+          };
+          KeepAlive = true;
+          ProgramArguments = [
+            (lib.getExe pkgs.unofficial.t3code)
+            "serve"
+            "--host"
+            "0.0.0.0"
+          ];
+          RunAtLoad = true;
+          WorkingDirectory = config.home.homeDirectory;
+        };
+      };
+    };
 }

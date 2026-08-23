b1189433a8bcd47959ac8b5fcf90f519bb3f024c
Khue Doan <mail@khuedoan.com>
2026-08-23 12:43:28 +0700

    refactor(dotfiles): use out of store symlinks
    Allow trying out new config faster without running nixos-rebuild switch.
    This improves iteration speed but provides fewer Nix guarantees.

--- modules/dotfiles/default.nix
+++ modules/dotfiles/default.nix
@@ -1,26 +1,35 @@
 {
   config,
   lib,
-  pkgs,
   ...
 }:
 
 let
-  dotfiles = lib.mapAttrs' (
-    name: type:
-    lib.nameValuePair name {
-      source = ./home + "/${name}";
-      recursive = type == "directory";
-    }
-  ) (builtins.readDir ./home);
+  username = config.primaryUser.username;
+  sourceRoot = ./home;
+
+  relativeFiles = map (lib.path.removePrefix sourceRoot) (
+    lib.filesystem.listFilesRecursive sourceRoot
+  );
 in
 {
-  config.home-manager.users.${config.primaryUser.username} = {
-    home.file = dotfiles // {
-      "Pictures/Wallpapers/astronaut-jellyfish.jpg".source = builtins.fetchurl {
-        url = "https://github.com/user-attachments/assets/b63195d0-7fe3-4ab5-95c7-20127123836c";
-        sha256 = "1g120j4z6665j4wh2g84m4rb24gvzdxyhx9lqym68cwn8ny2j7fz";
+  config.home-manager.users.${username} =
+    { config, ... }:
+
+    let
+      checkoutRoot = "${config.home.homeDirectory}/Projects/dotfiles/modules/dotfiles/home";
+
+      dotfiles = lib.genAttrs relativeFiles (relativePath: {
+        # This improves iteration speed but provides fewer Nix guarantees.
+        source = config.lib.file.mkOutOfStoreSymlink "${checkoutRoot}/${relativePath}";
+      });
+    in
+    {
+      home.file = dotfiles // {
+        "Pictures/Wallpapers/astronaut-jellyfish.jpg".source = builtins.fetchurl {
+          url = "https://github.com/user-attachments/assets/b63195d0-7fe3-4ab5-95c7-20127123836c";
+          sha256 = "1g120j4z6665j4wh2g84m4rb24gvzdxyhx9lqym68cwn8ny2j7fz";
+        };
       };
     };
-  };
 }
